59fbbcfd6d988ff47ab4b164253c9624e1bd3a2e
Takudzwa Allen Mushai <34072789+Takudzwa30@users.noreply.github.com>
2022-10-24 19:59:57 +0400

    add

--- script.js
+++ script.js
@@ -1,12 +1,45 @@
-'use strict';
+"use strict";
 
 // prettier-ignore
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
-const form = document.querySelector('.form');
-const containerWorkouts = document.querySelector('.workouts');
-const inputType = document.querySelector('.form__input--type');
-const inputDistance = document.querySelector('.form__input--distance');
-const inputDuration = document.querySelector('.form__input--duration');
-const inputCadence = document.querySelector('.form__input--cadence');
-const inputElevation = document.querySelector('.form__input--elevation');
+const form = document.querySelector(".form");
+const containerWorkouts = document.querySelector(".workouts");
+const inputType = document.querySelector(".form__input--type");
+const inputDistance = document.querySelector(".form__input--distance");
+const inputDuration = document.querySelector(".form__input--duration");
+const inputCadence = document.querySelector(".form__input--cadence");
+const inputElevation = document.querySelector(".form__input--elevation");
+
+if (navigator.geolocation)
+  navigator.geolocation.getCurrentPosition(
+    (position) => {
+      console.log(position);
+
+      //destructuring, this creates longitude and latitude variable based on the coords object, basically it takes the values as they are
+      const { longitude } = position.coords;
+      const { latitude } = position.coords;
+
+      console.log(longitude, latitude);
+
+      const coords = [latitude, longitude];
+
+      const map = L.map("map").setView(coords, 13);
+
+      L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.fr/hot/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker(coords)
+        .addTo(map)
+        .bindPopup("A pretty CSS3 popup.<br> Easily customizable.")
+        .openPopup();
+
+
+
+    },
+    () => {
+      alert("Couldn't get current position");
+    }
+  );
